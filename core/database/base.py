14c1d3b5d0e3d39c4319d4320f045154bed18a7f
SolanoJason <pajason2000@outlook.com>
2026-04-11 20:33:32 -0500

    g

---
+++ core/database/base.py
@@ -0,0 +1,15 @@
+from sqlalchemy import URL, create_engine, make_url
+from core.settings import settings
+
+url = make_url(settings.DB.URI.unicode_string())
+
+engine = create_engine(
+    url,
+    echo=True,
+    echo_pool=True,
+    pool_size=5,
+    max_overflow=10,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+    pool_timeout=15,
+)
---
+++ core/settings/base.py
@@ -0,0 +1,62 @@
+from pydantic import BaseModel, computed_field, PostgresDsn, model_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Literal, Self
+import urllib.parse
+
+
+class DatabaseQuerySettings(BaseModel):
+    SSLMODE: Literal[
+        "disable", "allow", "prefer", "require", "verify-ca", "verify-full"
+    ] = "prefer"
+    APPLICATION_NAME: str = "project-name"
+
+
+class DatabaseSettings(BaseModel):
+    DRIVER: str = "postgresql+psycopg"
+    HOST: str = "localhost"
+    USER: str = "postgres"
+    PORT: int = 5432
+    PASSWORD: str
+    NAME: str
+
+    QUERY: DatabaseQuerySettings = DatabaseQuerySettings()
+
+    @computed_field
+    @property
+    def URI(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme=self.DRIVER,
+            host=self.HOST,
+            password=self.PASSWORD,
+            username=self.USER,
+            port=self.PORT,
+            path=self.NAME,
+            query=urllib.parse.urlencode(
+                {k.lower(): v for k, v in settings.DB.QUERY.model_dump().items()}
+            ),
+        )
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="forbid",
+        frozen=True,
+        case_sensitive=True,
+        env_file_encoding="utf-8",
+        env_ignore_empty=True,
+        env_nested_delimiter="__",
+    )
+
+    DEBUG: bool
+
+    DB: DatabaseSettings
+
+    @model_validator(mode="after")
+    def check_environment(self) -> Self:
+        if not self.DEBUG and self.DB.QUERY.SSLMODE in ("disable", "allow", "prefer"):
+            raise ValueError("SSL is required in production")
+        return self
+
+
+settings = Settings()  # type: ignore
